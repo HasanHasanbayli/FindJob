99caff3bcbe114aca9b77e3443cc7cfe9f0364a8
HasanHasanbayli <hasanhasanbeyli@gmail.com>
2020-11-05 23:57:46 +0400

    first commit

---
+++ FindJob/Controllers/SeekersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindJob.Controllers
+{
+    public class SeekersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult JobList()
+        {
+            return View();
+        }
+        public IActionResult StaredJobs()
+        {
+            return View();
+        }
+        public IActionResult JobDetail()
+        {
+            return View();
+        }
+        public IActionResult UpdateProfile()
+        {
+            return View();
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
